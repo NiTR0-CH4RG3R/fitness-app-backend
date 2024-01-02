54781ada9f20351e7dab71ac27c8795c93fea2d0
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-02 23:08:51 +0530

    Initial Commit

---
+++ src/test/java/com/devgrp/fitnesswebapp/FitnessWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgrp.fitnesswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
