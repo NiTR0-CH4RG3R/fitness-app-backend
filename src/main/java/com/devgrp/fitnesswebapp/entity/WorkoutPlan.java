824ddeea51354f531111ebf6dcc26cdbe06b0738
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-05 00:51:38 +0530

    Added @Entity and lombok data annotations

--- src/main/java/com/devgrp/fitnesswebapp/entity/WorkoutPlan.java
+++ src/main/java/com/devgrp/fitnesswebapp/entity/WorkoutPlan.java
@@ -1,11 +1,18 @@
 package com.devgrp.fitnesswebapp.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.concurrent.TimeUnit;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class WorkoutPlan {
     @Id
     @GeneratedValue
