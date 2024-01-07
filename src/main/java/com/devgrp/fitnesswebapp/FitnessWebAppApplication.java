6bdf702daa748112e67241b9f6f81b6f29c2192a
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-08 00:55:15 +0530

    Nothing much changed. Just a small code to test a repository. I have commented them out. We can properly test out our repositories once we created controllers.

--- src/main/java/com/devgrp/fitnesswebapp/FitnessWebAppApplication.java
+++ src/main/java/com/devgrp/fitnesswebapp/FitnessWebAppApplication.java
@@ -1,13 +1,32 @@
 package com.devgrp.fitnesswebapp;
 
+import com.devgrp.fitnesswebapp.entity.Exercise;
+import com.devgrp.fitnesswebapp.repository.ExerciseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
-public class FitnessWebAppApplication {
+public class FitnessWebAppApplication /*implements CommandLineRunner*/ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessWebAppApplication.class, args);
 	}
 
+//	@Autowired
+//	ExerciseRepository exerciseRepository;
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Exercise e = new Exercise();
+//		e.setName("New Exercise");
+//		e.setDescription("Some Description");
+//		e.setTargetMuscle(new ArrayList<>());
+//		e.getTargetMuscle().add("SomeTargetMuscle");
+//		exerciseRepository.save(e);
+//	}
 }
