4696d49be62f0285f20eb6378aca44d73c947bee
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-07 16:48:08 +0530

    WTF InteliJ?

--- src/main/java/com/devgrp/fitnesswebapp/entity/UserLoggingLog.java
+++ src/main/java/com/devgrp/fitnesswebapp/entity/UserLoggingLog.java
@@ -4,9 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
