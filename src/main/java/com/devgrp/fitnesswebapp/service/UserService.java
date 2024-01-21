bffe8503a1970fd6a4dd96fb2ac266739c48ea55
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-21 08:44:47 +0530

    added the logics for add,update,search,delete and getall

--- src/main/java/com/devgrp/fitnesswebapp/service/UserService.java
+++ src/main/java/com/devgrp/fitnesswebapp/service/UserService.java
@@ -6,8 +6,13 @@ import com.devgrp.fitnesswebapp.repository.UserRepository;
 import com.devgrp.fitnesswebapp.util.VarList;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -23,6 +28,38 @@ public class UserService {
         else{
             userRepository.save(modelMapper.map(userDTO,User.class));
             return VarList.RSP_SUCCESS;
+        }
+    }
+    public String updateUser(UserDTO userDTO){
+        if(userRepository.existsById(userDTO.getId())){
+            userRepository.save(modelMapper.map(userDTO,User.class));
+            return VarList.RSP_SUCCESS;
+        }
+        else{
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
+
+    public List<UserDTO> getAllUsers(){
+        List<User> userList=userRepository.findAll();
+        return modelMapper.map(userList,new TypeToken<ArrayList<UserDTO>>(){}.getType());
+    }
+    public UserDTO searchUser(int userID){
+        if(userRepository.existsById(userID)){
+            User user=userRepository.findById(userID).orElse(null);
+            return modelMapper.map(user,UserDTO.class);
+        }
+        else {
+            return null;
+        }
+    }
+    public String deleteUser( int userID){
+        if(userRepository.existsById(userID)){
+            userRepository.deleteById(userID);
+            return VarList.RSP_SUCCESS;
+        }
+        else{
+            return VarList.RSP_NO_DATA_FOUND;
         }
     }
 }
