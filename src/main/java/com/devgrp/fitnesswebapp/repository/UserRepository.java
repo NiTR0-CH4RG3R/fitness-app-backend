1b6d8332ef1dc441b49980e0ce45a8ae8c0011f8
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-21 08:44:14 +0530

    nothing changed

--- src/main/java/com/devgrp/fitnesswebapp/repository/UserRepository.java
+++ src/main/java/com/devgrp/fitnesswebapp/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.devgrp.fitnesswebapp.repository;
 
 import com.devgrp.fitnesswebapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {}
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
