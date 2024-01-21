934836703f5e7ef7d22365e86d8c60dbd14d28e0
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-22 02:03:37 +0530

    created for get the exercise data

---
+++ src/main/java/com/devgrp/fitnesswebapp/dto/ExerciseGetDTO.java
@@ -0,0 +1,11 @@
+package com.devgrp.fitnesswebapp.dto;
+
+public class ExerciseGetDTO {
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private String gifURL;
+}
