ad83c3590fb713d22b2081cce9290aaf2172c8af
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-22 09:26:10 +0530

    added a setter to truncate description

--- src/main/java/com/devgrp/fitnesswebapp/dto/ExerciseGetDTO.java
+++ src/main/java/com/devgrp/fitnesswebapp/dto/ExerciseGetDTO.java
@@ -1,11 +1,26 @@
 package com.devgrp.fitnesswebapp.dto;
 
+import jakarta.persistence.Column;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Data
+@Getter
 public class ExerciseGetDTO {
+    @Setter
     private Integer id;
 
+    public void setDescription(String description) {
+        this.description = description.substring(0,100);//[TODO]:find a best way than this
+    }
+
+    @Setter
     private String name;
 
     private String description;
 
+    @Setter
     private String gifURL;
+
 }
