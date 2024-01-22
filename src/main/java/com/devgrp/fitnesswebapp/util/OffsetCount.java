60e7b6566655d991a3f8b060d785c89e5e96791c
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-22 15:49:36 +0530

    created PageElemnt for pass PageNo and NoOfElemnts

--- src/main/java/com/devgrp/fitnesswebapp/util/OffsetCount.java
+++
@@ -1,9 +0,0 @@
-package com.devgrp.fitnesswebapp.util;
-
-import lombok.Data;
-
-@Data
-public class OffsetCount {
-    private int offset;
-    private int count;
-}
---
+++ src/main/java/com/devgrp/fitnesswebapp/util/PageElement.java
@@ -0,0 +1,9 @@
+package com.devgrp.fitnesswebapp.util;
+
+import lombok.Data;
+
+@Data
+public class PageElement {
+    private int page;
+    private int noOfElements;
+}
