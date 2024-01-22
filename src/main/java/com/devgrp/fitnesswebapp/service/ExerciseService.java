3ee22026bea464649dd248c09acfd1d0a610b3e9
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-22 15:48:20 +0530

    used Pagable to retrive exercise

--- src/main/java/com/devgrp/fitnesswebapp/service/ExerciseService.java
+++ src/main/java/com/devgrp/fitnesswebapp/service/ExerciseService.java
@@ -1,18 +1,18 @@
 package com.devgrp.fitnesswebapp.service;
-
 import com.devgrp.fitnesswebapp.dto.ExerciseDTO;
 import com.devgrp.fitnesswebapp.dto.ExerciseGetDTO;
-import com.devgrp.fitnesswebapp.dto.UserDTO;
 import com.devgrp.fitnesswebapp.entity.Exercise;
-import com.devgrp.fitnesswebapp.entity.User;
+import com.devgrp.fitnesswebapp.repository.ExercisePageRepository;
 import com.devgrp.fitnesswebapp.repository.ExerciseRepository;
 import com.devgrp.fitnesswebapp.util.VarList;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +21,8 @@ import java.util.List;
 public class ExerciseService {
     @Autowired
     private ExerciseRepository exerciseRepository;
+    @Autowired
+    private ExercisePageRepository exercisePageRepository;
     @Autowired
     private ModelMapper modelMapper;
     public String addExercise(ExerciseDTO exerciseDTO) {
@@ -32,12 +34,9 @@ public class ExerciseService {
             return VarList.RSP_SUCCESS;
         }
     }
-    public List<ExerciseGetDTO> getExercise(int offset,int count){
-        ArrayList<Exercise> exerciseList=new ArrayList<Exercise>(count);
-        for(int i=offset;i<=offset+count;i++) {
-           // exerciseList[i]=exerciseRepository.findById(i).orElse(null);
-            exerciseList.add(exerciseList.size(), exerciseRepository.findById(i).orElse(null));//[TODO]:find the best way to get data from db
-        }
+    public List<ExerciseGetDTO> getExercise(int pageNo,int noOfElemnts){
+        Pageable page= (Pageable) PageRequest.of(pageNo,noOfElemnts);
+        List<Exercise> exerciseList=exercisePageRepository.findAll(page);
         return modelMapper.map(exerciseList,new TypeToken<ArrayList<ExerciseGetDTO>>(){}.getType());
     }
     public long getExerciseCount(){
