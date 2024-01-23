a763145052e458df4e9f849f7266af2c667c4f13
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-23 10:50:31 +0530

    updated with UserGetDTO

--- src/main/java/com/devgrp/fitnesswebapp/controller/UserController.java
+++ src/main/java/com/devgrp/fitnesswebapp/controller/UserController.java
@@ -2,8 +2,10 @@ package com.devgrp.fitnesswebapp.controller;
 
 import com.devgrp.fitnesswebapp.dto.ResponseDTO;
 import com.devgrp.fitnesswebapp.dto.UserDTO;
+import com.devgrp.fitnesswebapp.dto.UserGetDTO;
 import com.devgrp.fitnesswebapp.service.UserService;
 import com.devgrp.fitnesswebapp.util.VarList;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,116 +16,117 @@ import java.util.List;
 @RestController
 @CrossOrigin
 @RequestMapping(value = "api/v1/user")
+@AllArgsConstructor
 public class UserController {
     @Autowired
     private UserService userService;
     @Autowired
     private ResponseDTO responseDTO;
     @PatchMapping(value = "/add")
-    public ResponseEntity addUser(@RequestBody UserDTO userDTO){
+    public ResponseEntity<ResponseDTO> addUser(@RequestBody UserDTO userDTO){
         try{
             String res=userService.createUser(userDTO);
             if(res.equals("00")){
                 responseDTO.setCode(VarList.RSP_SUCCESS);
                 responseDTO.setMessage("Succesfully saved");
                 responseDTO.setContent(userDTO);
-                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+                return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
             } else if (res.equals("06")) {
                 responseDTO.setCode(VarList.RSP_DUPLICATED);
                 responseDTO.setMessage("Allready added");
                 responseDTO.setContent(null);
-                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
             }
             else {
                 responseDTO.setCode(VarList.RSP_ERROR);
                 responseDTO.setMessage("Error");
                 responseDTO.setContent(null);
-                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
             }
         }
         catch (Exception ex){
             responseDTO.setCode(VarList.RSP_ERROR);
             responseDTO.setMessage(ex.getMessage());
             responseDTO.setContent(null);
-            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
     }
     @GetMapping(value = "/getAll")
-    public ResponseEntity getAllUsers(){
+    public ResponseEntity<ResponseDTO> getAllUsers(){
         try{
-            List<UserDTO> userDTOList=userService.getAllUsers();
+            List<UserGetDTO> userGetDTOList=userService.getAllUsers();
             responseDTO.setCode(VarList.RSP_SUCCESS);
             responseDTO.setMessage("success");
-            responseDTO.setContent(userDTOList);
-            return new ResponseEntity(responseDTO,HttpStatus.ACCEPTED);
+            responseDTO.setContent(userGetDTOList);
+            return new ResponseEntity<>(responseDTO,HttpStatus.ACCEPTED);
         }catch (Exception ex){
             responseDTO.setCode(VarList.RSP_ERROR);
             responseDTO.setMessage(ex.getMessage());
             responseDTO.setContent(null);
-            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     @PutMapping(value="/update")
-    public ResponseEntity updateUser(@RequestBody UserDTO userDTO){
+    public ResponseEntity<ResponseDTO> updateUser(@RequestBody UserDTO userDTO){
         try{
             String res=userService.updateUser(userDTO);
             if(res.equals("00")){
                 responseDTO.setCode(VarList.RSP_SUCCESS);
                 responseDTO.setMessage("Succesfully updated");//[TODO]:edit code to update user
                 responseDTO.setContent(userDTO);
-                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+                return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
             } else{
                 responseDTO.setCode(VarList.RSP_NO_DATA_FOUND);
                 responseDTO.setMessage("User Not Found");
                 responseDTO.setContent(null);
-                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
             }
         }
         catch (Exception ex){
             responseDTO.setCode(VarList.RSP_ERROR);
             responseDTO.setMessage(ex.getMessage());
             responseDTO.setContent(null);
-            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    @GetMapping(value = "/search/{userID}")
-    public ResponseEntity searchUser(@PathVariable int userID){
-        UserDTO userDTO=userService.searchUser(userID);
-        if(userDTO!=null){
+    @GetMapping(value = "/search/{userEmail}")
+    public ResponseEntity<ResponseDTO> searchUser(@PathVariable String userEmail){
+        UserGetDTO userGetDTO=userService.searchUser(userEmail);
+        if(userGetDTO!=null){
             responseDTO.setCode(VarList.RSP_SUCCESS);
             responseDTO.setMessage("Success");
-            responseDTO.setContent(userDTO);
-            return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            responseDTO.setContent(userGetDTO);
+            return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
         }
         else{
             responseDTO.setCode(VarList.RSP_NO_DATA_FOUND);
             responseDTO.setMessage("User not Found");
-            responseDTO.setContent(userDTO);
-            return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            responseDTO.setContent(null);
+            return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
         }
     }
-    @DeleteMapping(value = "/delete/{userID}")
-    public ResponseEntity deleteUser(@PathVariable int userID){
+    @DeleteMapping(value = "/delete/{userEmail}")
+    public ResponseEntity<ResponseDTO> deleteUser(@PathVariable String userEmail){
         try{
-            String res=userService.deleteUser(userID);
+            String res=userService.deleteUser(userEmail);
             if(res.equals("00")){
                 responseDTO.setCode(VarList.RSP_SUCCESS);
                 responseDTO.setMessage("Succesfully deleted");
                 responseDTO.setContent(null);
-                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+                return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
             } else{
                 responseDTO.setCode(VarList.RSP_NO_DATA_FOUND);
                 responseDTO.setMessage("User Not Found");
                 responseDTO.setContent(null);
-                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
             }
         }
         catch (Exception ex){
             responseDTO.setCode(VarList.RSP_ERROR);
             responseDTO.setMessage(ex.getMessage());
             responseDTO.setContent(null);
-            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
