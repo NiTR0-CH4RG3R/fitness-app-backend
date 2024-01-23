c1df42e5d3fe874d0caafd68ec0d967fce34f09a
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-23 10:40:37 +0530

    added new methods

--- src/main/java/com/devgrp/fitnesswebapp/repository/UserRepository.java
+++ src/main/java/com/devgrp/fitnesswebapp/repository/UserRepository.java
@@ -2,12 +2,16 @@ package com.devgrp.fitnesswebapp.repository;
 
 import com.devgrp.fitnesswebapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByEmail(String email);
+    void deleteByEmail(String email);
+
+    Optional<User> findUserByEmail(String email);
 
 }
