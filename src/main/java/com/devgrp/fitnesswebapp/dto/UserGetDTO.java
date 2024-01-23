205324ba6541a1c19a932c3ef41d93c6d35e9807
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-23 10:40:05 +0530

    new DTO created for getting data

---
+++ src/main/java/com/devgrp/fitnesswebapp/dto/UserGetDTO.java
@@ -0,0 +1,30 @@
+package com.devgrp.fitnesswebapp.dto;
+
+import com.devgrp.fitnesswebapp.entity.WorkoutPlan;
+import com.devgrp.fitnesswebapp.entity.types.UserType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserGetDTO {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private UserType type =UserType.USER;
+    private String email;
+    private LocalDate dob;
+    private String address;
+    private String telephoneNo;
+    private float weight;
+    private String bloodGroup;
+    private String healthIssues;
+    private String emergencyContact;
+    private WorkoutPlan followingWorkoutPlan=null;
+    private boolean goalNotification ;
+    private boolean workoutPlanNotification;
+    private boolean otherNotification;
+}
