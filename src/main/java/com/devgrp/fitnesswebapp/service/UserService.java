0224bcbb9c897dbdbad950e060b4335b00e8e68e
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-23 10:43:48 +0530

    updated with UserDTO and UserGetDTO

--- src/main/java/com/devgrp/fitnesswebapp/service/UserService.java
+++ src/main/java/com/devgrp/fitnesswebapp/service/UserService.java
@@ -1,28 +1,32 @@
 package com.devgrp.fitnesswebapp.service;
 
 import com.devgrp.fitnesswebapp.dto.UserDTO;
+import com.devgrp.fitnesswebapp.dto.UserGetDTO;
 import com.devgrp.fitnesswebapp.entity.User;
 import com.devgrp.fitnesswebapp.repository.UserRepository;
 import com.devgrp.fitnesswebapp.util.VarList;
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Transactional
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private ModelMapper modelMapper;
     public String createUser(UserDTO userDTO){
-        if(userRepository.existsById(userDTO.getId())){
+        if(userRepository.existsByEmail(userDTO.getEmail())){
             return VarList.RSP_DUPLICATED;
         }
         else{
@@ -31,7 +35,7 @@ public class UserService {
         }
     }
     public String updateUser(UserDTO userDTO){
-        if(userRepository.existsById(userDTO.getId())){
+        if(userRepository.existsByEmail(userDTO.getEmail())){
             userRepository.save(modelMapper.map(userDTO,User.class));
             return VarList.RSP_SUCCESS;
         }
@@ -40,22 +44,22 @@ public class UserService {
         }
     }
 
-    public List<UserDTO> getAllUsers(){
+    public List<UserGetDTO> getAllUsers(){
         List<User> userList=userRepository.findAll();
-        return modelMapper.map(userList,new TypeToken<ArrayList<UserDTO>>(){}.getType());
+        return modelMapper.map(userList,new TypeToken<ArrayList<UserGetDTO>>(){}.getType());
     }
-    public UserDTO searchUser(int userID){
-        if(userRepository.existsById(userID)){
-            User user=userRepository.findById(userID).orElse(null);
-            return modelMapper.map(user,UserDTO.class);
+    public UserGetDTO searchUser(String userEmail){
+        if(userRepository.existsByEmail(userEmail)){
+            User user=userRepository.findUserByEmail(userEmail).orElse(null);
+            return modelMapper.map(user,UserGetDTO.class);
         }
         else {
             return null;
         }
     }
-    public String deleteUser( int userID){
-        if(userRepository.existsById(userID)){
-            userRepository.deleteById(userID);
+    public String deleteUser( String userEmail){
+        if(userRepository.existsByEmail(userEmail)){
+            userRepository.deleteByEmail(userEmail);
             return VarList.RSP_SUCCESS;
         }
         else{
