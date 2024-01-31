98d2e21ed37c9b0c33dc78bce52967a97382fb93
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-31 19:16:31 +0530

    added some methods for ExerciseRepository

--- src/main/java/com/devgrp/fitnesswebapp/repository/ExerciseRepository.java
+++ src/main/java/com/devgrp/fitnesswebapp/repository/ExerciseRepository.java
@@ -4,5 +4,11 @@ import com.devgrp.fitnesswebapp.entity.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ExerciseRepository extends JpaRepository<Exercise, Integer> {}
+public interface ExerciseRepository extends JpaRepository<Exercise, Integer> {
+    boolean existsByName(String name);
+
+    Optional<Exercise> findByName(String name);
+}
