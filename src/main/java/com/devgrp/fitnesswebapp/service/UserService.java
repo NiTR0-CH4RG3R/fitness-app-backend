fd088f234ee9126dd072ac2f1d47777103550a5f
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-31 19:22:20 +0530

    implemnted methods for retrive and store goals

--- src/main/java/com/devgrp/fitnesswebapp/service/UserService.java
+++ src/main/java/com/devgrp/fitnesswebapp/service/UserService.java
@@ -1,8 +1,12 @@
 package com.devgrp.fitnesswebapp.service;
 
+import com.devgrp.fitnesswebapp.dto.GoalDTO;
+import com.devgrp.fitnesswebapp.dto.GoalGetDTO;
 import com.devgrp.fitnesswebapp.dto.UserDTO;
 import com.devgrp.fitnesswebapp.dto.UserGetDTO;
+import com.devgrp.fitnesswebapp.entity.Goal;
 import com.devgrp.fitnesswebapp.entity.User;
+import com.devgrp.fitnesswebapp.repository.GoalRepository;
 import com.devgrp.fitnesswebapp.repository.UserRepository;
 import com.devgrp.fitnesswebapp.util.VarList;
 import jakarta.transaction.Transactional;
@@ -25,6 +29,8 @@ public class UserService {
     private UserRepository userRepository;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private GoalRepository goalRepository;
     public String createUser(UserDTO userDTO){
         if(userRepository.existsByEmail(userDTO.getEmail())){
             return VarList.RSP_DUPLICATED;
@@ -64,6 +70,44 @@ public class UserService {
         }
         else{
             return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
+    public String addGoal(String userEmail,GoalDTO goalDTO) {
+        try {
+            if (userRepository.existsByEmail(userEmail)){
+                User user=userRepository.findUserByEmail(userEmail).orElse(null);
+                Goal goal=modelMapper.map(goalDTO, Goal.class);
+                assert user != null;
+                if(user.getGoals()==null){
+                    ArrayList<Goal> goals= new ArrayList<>();
+                    goals.add(goal);
+                }
+                else {
+                    user.getGoals().add(user.getGoals().size(),goal);
+                }
+                userRepository.save(user);
+                return VarList.RSP_SUCCESS;
+            } else {
+                return VarList.RSP_ERROR;
+            }
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+    public List<GoalGetDTO> getAllGoals(String userEmail){
+        User user = userRepository.findUserByEmail(userEmail).orElse(null);
+        if ( user == null ) return null;
+        List<Goal> goalList = user.getGoals();
+        if ( goalList == null ) return null;
+        return modelMapper.map( goalList, new TypeToken<ArrayList<GoalGetDTO>> () {}.getType());
+    }
+    public GoalGetDTO searchGoal(String userEmail,String goalName){
+        User user = userRepository.findUserByEmail(userEmail).orElse(null);
+        if ( user == null ) return null;
+        List<Goal> goalList = user.getGoals();
+        for(int i=0;i<goalList.size();i++){
+            if(goalList.contains(goalName))
+                //[TODO]:complete by getting from the goalDTO
         }
     }
 }
