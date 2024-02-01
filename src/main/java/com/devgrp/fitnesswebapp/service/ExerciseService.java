f29fee1826f663844b19e311370675f63f453378
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-02-02 00:21:20 +0530

    created service for adding exerciseUserReview

--- src/main/java/com/devgrp/fitnesswebapp/service/ExerciseService.java
+++ src/main/java/com/devgrp/fitnesswebapp/service/ExerciseService.java
@@ -1,8 +1,11 @@
 package com.devgrp.fitnesswebapp.service;
 import com.devgrp.fitnesswebapp.dto.ExerciseDTO;
 import com.devgrp.fitnesswebapp.dto.ExerciseGetDTO;
-import com.devgrp.fitnesswebapp.entity.Exercise;
-import com.devgrp.fitnesswebapp.repository.ExerciseRepository;
+import com.devgrp.fitnesswebapp.dto.UserReviewDTO;
+import com.devgrp.fitnesswebapp.dto.UserReviewGetDTO;
+import com.devgrp.fitnesswebapp.entity.*;
+import com.devgrp.fitnesswebapp.entity.compositekeys.ExerciseUserReviewKey;
+import com.devgrp.fitnesswebapp.repository.*;
 import com.devgrp.fitnesswebapp.util.VarList;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
@@ -20,6 +23,13 @@ public class ExerciseService {
     private ExerciseRepository exerciseRepository;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private WorkoutPlanRepository workoutPlanRepository;
+    @Autowired
+    private ExerciseUserReviewRepository exerciseUserReviewRepository;
+
     public String addExercise(ExerciseDTO exerciseDTO) {
         if (exerciseRepository.existsByName(exerciseDTO.getName())){
             return VarList.RSP_DUPLICATED;
@@ -59,5 +69,39 @@ public class ExerciseService {
         else {
             return null;
         }
+    }
+    public String addExerciseReview(String userEmail,UserReviewDTO userReviewDTO){
+        try{
+            User user=userRepository.findUserByEmail(userEmail).orElse(null);
+            if(user==null) return VarList.RSP_NO_DATA_FOUND;
+            Exercise exercise=exerciseRepository.findById(userReviewDTO.getEntityId()).orElse(null);
+            if (exercise==null) return VarList.RSP_NO_DATA_FOUND;
+            ExerciseUserReview exerciseUserReview=new ExerciseUserReview();
+            exerciseUserReview.setUser(user);
+            exerciseUserReview.setExercise(exercise);
+            exerciseUserReview.setComment(userReviewDTO.getComment());
+            exerciseUserReview.setRating(userReviewDTO.getRating());
+            exerciseUserReviewRepository.save(exerciseUserReview);
+            return VarList.RSP_SUCCESS;
+        }
+        catch (Exception ex){
+            return VarList.RSP_ERROR;
+        }
+    }
+    public List<UserReviewGetDTO> getExerciseUserReview(){
+        try{
+            List<ExerciseUserReview> exerciseUserReviewList=exerciseUserReviewRepository.findAll();
+            return modelMapper.map(exerciseUserReviewList,new TypeToken<ArrayList<UserReviewDTO>>(){}.getType());
+        }
+        catch (Exception ex){
+            return null;
+        }
+
+    }
+    public String deleteExerciseReview(ExerciseUserReviewKey exerciseReviewId){
+        ExerciseUserReview exerciseUserReview=exerciseUserReviewRepository.findById(exerciseReviewId).orElse(null);
+        if(exerciseUserReview==null) return VarList.RSP_NO_DATA_FOUND;
+        exerciseUserReviewRepository.delete(exerciseUserReview);
+        return VarList.RSP_SUCCESS;
     }
 }
