4c638a2c9b4f4e7bc2b5541229137d40e3eda153
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-02-02 00:22:13 +0530

    added ExerciseReview controller

--- src/main/java/com/devgrp/fitnesswebapp/controller/ExerciseController.java
+++ src/main/java/com/devgrp/fitnesswebapp/controller/ExerciseController.java
@@ -4,14 +4,13 @@ import com.devgrp.fitnesswebapp.dto.ExerciseDTO;
 import com.devgrp.fitnesswebapp.dto.ExerciseGetDTO;
 import com.devgrp.fitnesswebapp.dto.ResponseDTO;
 import com.devgrp.fitnesswebapp.service.ExerciseService;
+import com.devgrp.fitnesswebapp.util.EmailReview;
 import com.devgrp.fitnesswebapp.util.PageElement;
 import com.devgrp.fitnesswebapp.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
@@ -53,7 +52,7 @@ public class ExerciseController {
     }
 
     @GetMapping(value = "/get")
-    public ResponseEntity<ResponseDTO> getExercise( @RequestBody PageElement pageElement) {
+    public ResponseEntity<ResponseDTO> getExercise(@RequestBody PageElement pageElement) {
         try {
             List<ExerciseGetDTO> exerciseGetDTOList = exerciseService.getExercise(pageElement.getPage(), pageElement.getNoOfElements());
             responseDTO.setCode(VarList.RSP_SUCCESS);
@@ -83,6 +82,7 @@ public class ExerciseController {
             return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
     @PutMapping(value = "/update")
     public ResponseEntity<ResponseDTO> updateExercise(@RequestBody ExerciseGetDTO exerciseGetDTO) {
         try {
@@ -110,29 +110,30 @@ public class ExerciseController {
             return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
     @GetMapping(value = "/search/{exerciseName}")
-    public ResponseEntity<ResponseDTO> searchExercise(@PathVariable String exerciseName){
-        try{
-            ExerciseGetDTO exerciseGetDTO=exerciseService.searchExercise(exerciseName);
-            if(exerciseGetDTO!=null){
+    public ResponseEntity<ResponseDTO> searchExercise(@PathVariable String exerciseName) {
+        try {
+            ExerciseGetDTO exerciseGetDTO = exerciseService.searchExercise(exerciseName);
+            if (exerciseGetDTO != null) {
                 responseDTO.setCode(VarList.RSP_SUCCESS);
                 responseDTO.setMessage("Success");
                 responseDTO.setContent(exerciseGetDTO);
                 return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
-            }
-            else{
+            } else {
                 responseDTO.setCode(VarList.RSP_NO_DATA_FOUND);
                 responseDTO.setMessage("Exercise not found");
                 responseDTO.setContent(null);
                 return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             responseDTO.setCode(VarList.RSP_ERROR);
             responseDTO.setMessage(ex.getMessage());
             responseDTO.setContent(null);
             return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
     @DeleteMapping(value = "/delete/{exerciseName}")
     public ResponseEntity<ResponseDTO> deleteExercise(@PathVariable String exerciseName) {
         try {
@@ -148,6 +149,31 @@ public class ExerciseController {
                 responseDTO.setContent(null);
                 return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
             } else {
+                responseDTO.setCode(VarList.RSP_ERROR);
+                responseDTO.setMessage("Error");
+                responseDTO.setContent(null);
+                return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception ex) {
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //userReview
+    @PostMapping(value = "/addExerciseReview")
+    public ResponseEntity<ResponseDTO> addExerciseReview(@RequestBody EmailReview emailReview) {
+        try {
+            String res = exerciseService.addExerciseReview(emailReview.getUserEmail(),emailReview.getUserReviewDTO());
+            if (res.equals("00")) {
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(emailReview.getUserReviewDTO());
+                return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
+            }
+            else {
                 responseDTO.setCode(VarList.RSP_ERROR);
                 responseDTO.setMessage("Error");
                 responseDTO.setContent(null);
