31ed43e3be24ff24d041ee6818aaa66efebf4e01
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-02-01 19:03:10 +0530

    created logic for public workout plans

--- src/main/java/com/devgrp/fitnesswebapp/service/WorkoutPlanService.java
+++ src/main/java/com/devgrp/fitnesswebapp/service/WorkoutPlanService.java
@@ -1,22 +1,33 @@
 package com.devgrp.fitnesswebapp.service;
 
+import com.devgrp.fitnesswebapp.dto.ExerciseGetDTO;
+import com.devgrp.fitnesswebapp.dto.WorkoutPlanDTO;
 import com.devgrp.fitnesswebapp.dto.WorkoutPlanGetDTO;
 import com.devgrp.fitnesswebapp.entity.WorkoutPlan;
+import com.devgrp.fitnesswebapp.repository.WorkoutPlanRepository;
 import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Transactional
 public class WorkoutPlanService {
-
+    @Autowired
+    private WorkoutPlanRepository workoutPlanRepository;
+    @Autowired
+    private ModelMapper modelMapper;
     public List<WorkoutPlanGetDTO> getPublicWorkoutPlans(){
         try{
-            List<WorkoutPlanGetDTO> workoutPlanGetDTOList=
+            List<WorkoutPlan> workoutPlanList=workoutPlanRepository.findByIsPublic(true).orElse(null);
+            return modelMapper.map(workoutPlanList,new TypeToken<ArrayList<WorkoutPlanGetDTO>>(){}.getType());
         }
         catch (Exception ex){
-
+            return null;
         }
     }
 }
