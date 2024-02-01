9d3b288f2dc920dbeefca1dcfe4e5c1d27f8d623
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-02-01 19:03:53 +0530

    edited repository for getting public workoutplans

--- src/main/java/com/devgrp/fitnesswebapp/repository/WorkoutPlanRepository.java
+++ src/main/java/com/devgrp/fitnesswebapp/repository/WorkoutPlanRepository.java
@@ -5,9 +5,10 @@ import com.devgrp.fitnesswebapp.entity.WorkoutPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface WorkoutPlanRepository extends JpaRepository<WorkoutPlan, Integer> {
-
+    Optional<List<WorkoutPlan>> findByIsPublic(boolean isPublic);
 }
