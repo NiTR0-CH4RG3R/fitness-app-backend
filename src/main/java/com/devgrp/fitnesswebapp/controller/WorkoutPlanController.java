2f7046395993005e1cb24732965a8131edcced47
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-02-02 00:20:08 +0530

    added addWorkoutPlanReview controller

--- src/main/java/com/devgrp/fitnesswebapp/controller/WorkoutPlanController.java
+++ src/main/java/com/devgrp/fitnesswebapp/controller/WorkoutPlanController.java
@@ -1,16 +1,15 @@
 package com.devgrp.fitnesswebapp.controller;
 
-import com.devgrp.fitnesswebapp.dto.ExerciseGetDTO;
+
 import com.devgrp.fitnesswebapp.dto.ResponseDTO;
 import com.devgrp.fitnesswebapp.dto.WorkoutPlanGetDTO;
 import com.devgrp.fitnesswebapp.service.WorkoutPlanService;
+import com.devgrp.fitnesswebapp.util.EmailReview;
 import com.devgrp.fitnesswebapp.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +28,29 @@ public class WorkoutPlanController {
             responseDTO.setMessage("success");
             responseDTO.setContent(workoutPlanGetDTOList);
             return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PostMapping(value = "/addWorkoutPlanReview")
+    public ResponseEntity<ResponseDTO> addWorkoutPlanReview(@RequestBody EmailReview emailReview) {
+        try {
+            String res = workoutPlanService.addWorkoutPlanReview(emailReview.getUserEmail(),emailReview.getUserReviewDTO());
+            if (res.equals("00")) {
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(emailReview.getUserReviewDTO());
+                return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
+            }
+            else {
+                responseDTO.setCode(VarList.RSP_ERROR);
+                responseDTO.setMessage("Error");
+                responseDTO.setContent(null);
+                return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+            }
         } catch (Exception ex) {
             responseDTO.setCode(VarList.RSP_ERROR);
             responseDTO.setMessage(ex.getMessage());
